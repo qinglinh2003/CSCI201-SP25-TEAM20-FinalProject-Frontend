926a1b427ade2415fdedeaef7699b53a92700a56
Qinglin Hou <qinglinh@usc.edu>
2025-04-25 18:02:19 -0700

    add mode switcher

--- src/features/upload_file/UploadFile.jsx
+++ src/features/upload_file/UploadFile.jsx
@@ -1,13 +1,17 @@
 import {Link} from "react-router-dom";
-
+import styles from "./UploadFile.module.css";
 function UploadFile(){
     return(
-        <div>
+        <div className={styles.modalContainer}>
             <h1>Upload File</h1>
+            <div className={styles.modeSwitcher}>
+                <div></div>
+                <div></div>
+            </div>
             <Link to="/">Back</Link>
 
         </div>
-    )
+     )
 }
 
 export default UploadFile;
